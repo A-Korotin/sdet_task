393ef98eb3c63ad69aacba8fa170e3d527579b4e
Alexey Korotin <sx12fx@gmail.com>
2023-09-07 12:05:17 +0300

    Initial commit

---
+++ src/test/java/org/simbirsoft/sdet_task/SdetTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simbirsoft.sdet_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
