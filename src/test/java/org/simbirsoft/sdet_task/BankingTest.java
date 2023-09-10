b9041247d070cb92f7a9355d33fdad0297f6b284
Alexey Korotin <sx12fx@gmail.com>
2023-09-10 12:23:19 +0300

    test: remaining tests implementation

--- src/test/java/org/simbirsoft/sdet_task/BankingTest.java
+++ src/test/java/org/simbirsoft/sdet_task/BankingTest.java
@@ -6,9 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.simbirsoft.sdet_task.domain.Transaction;
+import org.simbirsoft.sdet_task.domain.TransactionType;
 import org.simbirsoft.sdet_task.pages.BankingPage;
+import org.simbirsoft.sdet_task.service.ExportService;
 import org.simbirsoft.sdet_task.service.FibonacciService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -16,6 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -33,6 +38,10 @@ public class BankingTest {
     @Autowired
     private FibonacciService fibonacciService;
 
+    @Autowired
+    @Qualifier("csvExporter")
+    private ExportService exportService;
+
     @BeforeAll
     public void setup() throws Exception {
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -49,30 +58,46 @@ public class BankingTest {
 
     @Test
     @Order(1)
-    public void login_ThenCheckBalance() {
+    public void login() {
         bankingPage.openLoginPage()
                 .loginAs("Harry Potter");
-
-        long balance = bankingPage.checkBalance();
-
-        Assertions.assertEquals(0, balance);
     }
 
     @Test
     @Order(2)
-    public void deposit_ThenWithdraw_ThenCheckBalance() {
-        int currentDayOfMonth = LocalDate.now().getDayOfMonth();
-        long amount = fibonacciService.calculateFibonacci(currentDayOfMonth);
-
-        bankingPage.deposit(amount);
-        bankingPage.withdraw(amount);
+    public void checkInitialBalance() {
         long balance = bankingPage.checkBalance();
         Assertions.assertEquals(0, balance);
     }
 
+    @Test
+    @Order(3)
+    public void deposit_ThenWithdraw_ThenCheckBalance() {
+        int currentDayOfMonth = LocalDate.now().getDayOfMonth();
+        long amount = fibonacciService.calculateFibonacci(currentDayOfMonth);
+
+        bankingPage
+                .deposit(amount)
+                .withdraw(amount);
+
+        long balance = bankingPage.checkBalance();
+        Assertions.assertEquals(0, balance);
+    }
+
+    @Test
+    @Order(4)
+    public void checkTransactions() {
+        List<Transaction> transactions = bankingPage.openTransactionsPage().retrieveTransactions();
+        Assertions.assertEquals(2, transactions.size());
+
+        Assertions.assertEquals(TransactionType.CREDIT, transactions.get(0).getType());
+        Assertions.assertEquals(TransactionType.DEBIT, transactions.get(1).getType());
+
+        exportService.export(transactions);
+    }
+
     @AfterAll
     public void teardown() {
-        bankingPage.openTransactionsPage().clearAndReturn();
         driver.quit();
     }
 }
